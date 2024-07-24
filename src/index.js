ae2a6970c40057cd2004ae7a27a5f1454e5bb5a0
Eyal Brilling <eyalbrilling@gmail.com>
2024-07-24 15:35:21 +0300

    herry picked circle logic
    (cherry picked from commit 63960faf91c9b1fed48259833bb8ac6df3de8da3)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
 
+var mousePos = view.center;
+var outerRadius= 50;
+var innerRadius= 10;
+var outerCircle = new Path.Circle({
+    center: mousePos,
+    radius: outerRadius,
+    strokeColor: 'black'
+});
 
-var myPath = new Path();
-myPath.strokeColor = 'black';
-myPath.add(new Point(0, 0));
-myPath.add(new Point(100, 50));
+var innerCircle = new Path.Circle({
+    center: view.center,
+    radius: innerRadius,
+    fillColor: 'purple'
+});
+
+view.onMouseMove = function(event){
+    mousePos = event.point;
+
+    outerCircle.position = mousePos;
+    var mouseAnchorDist = mousePos.subtract(innerCircle.position);
+    console.log(mouseAnchorDist.length);
+    if(mouseAnchorDist.length > outerRadius){
+        console.log("out");
+    innerCircle.position = distConstrant(outerCircle.center,innerCircle.center,outerRadius);
+    }
+}
+
+function distConstrant(center,anchorPoint,distance){
+    return ((anchorPoint.subtract(center))* distance) + anchorPoint;
+
+}
+
+
+// // Create a circle at the center of the canvas
+// var circle = new paper.Path.Circle({
+//     center: paper.view.center,
+//     radius: 50,
+//     fillColor: 'blue'
+// });
+
+// // Animation function
+// paper.view.onFrame = function(event) {
+//     circle.scale(Math.sin(event.count / 10) * 0.01 + 1);
+// };
