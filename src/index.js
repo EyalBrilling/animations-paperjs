1eda2ab6fb0c986818a474c6be7d941ee82acdb0
Eyal Brilling <eyalbrilling@gmail.com>
2024-07-25 14:17:39 +0300

    calling paperscript and the js file runs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import paper from 'paper';
 
-window.onload = function() {
-    paper.setup('myCanvas');
 
-    // Draw a circle at position (100, 100) with radius 50
-    var center = new paper.Point(100, 100);
-    var circle = new paper.Path.Circle(center, 50);
-    circle.fillColor = 'red';
-
-    // Optional: Animate the circle
-    paper.view.onFrame = function(event) {
-        circle.position.x += Math.sin(event.time * 2) * 2;
-        circle.position.y += Math.cos(event.time * 3) * 1;
-    };
-};
+var myPath = new Path();
+myPath.strokeColor = 'black';
+myPath.add(new Point(0, 0));
+myPath.add(new Point(100, 50));
